af15619e089e758acc52309a233d594b2881f464
宋冲 <chong.song@alibaba-inc.com>
2016-06-01 23:24:40 +0800

    修改包名

--- cron/cron_test.go
+++ cron/cron_test.go
@@ -1,22 +1,33 @@
 package cron_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 	"github.com/song316/go_cron/cron"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 const ONE_SECOND = 1 * time.Second + 10 * time.Millisecond
 
 func TestFuncJob_Run(t *testing.T) {
-	cron := cron.New()
-	cron.Start()
-	select {
-	case <-time.After(ONE_SECOND):
-		t.FailNow()
-	case <-stop(cron):
+	Convey("测试启动Job", t, func() {
+		cron := cron.New()
+		cron.Start()
+		select {
+		case <-time.After(ONE_SECOND):
+			t.FailNow()
+		case <-stop(cron):
+		}
+	})
+}
 
-	}
+func TestRun(t *testing.T) {
+	c := cron.New();
+	c.AddFunc("1 * * * * *", func() {
+		fmt.Println("每秒")
+	})
+	c.Start()
 }
 
 func stop(cron *cron.Cron) chan bool {
--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/song316/go_cron/cron"
 )
 
 func main() {
-	r := gin.Default()
-	r.POST("/", func(c *gin.Context) {
-		name := c.DefaultPostForm("name", "ryan")
-		age := c.DefaultPostForm("age", "1")
-		c.JSON(200, gin.H{
-			"name":name,
-			"age":age,
-		})
+	//r := gin.Default()
+	//r.POST("/", func(c *gin.Context) {
+	//	name := c.DefaultPostForm("name", "ryan")
+	//	age := c.DefaultPostForm("age", "1")
+	//	c.JSON(200, gin.H{
+	//		"name":name,
+	//		"age":age,
+	//	})
+	//})
+	//r.Run(":8080")
+
+	flag := make(chan bool)
+	c := cron.New();
+	c.AddFunc("*/1 * * * * *", func() {
+		fmt.Println("每秒")
 	})
-	r.Run(":8080")
+	c.Start()
+	<-flag
 }
