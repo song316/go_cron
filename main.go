6d951baaa35c13e7a9c455beec608d7273f3d6cf
宋冲 <chong.song@alibaba-inc.com>
2016-06-02 14:11:53 +0800

    xx

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"github.com/song316/go_cron/cron"
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//r := gin.Default()
-	//r.POST("/", func(c *gin.Context) {
-	//	name := c.DefaultPostForm("name", "ryan")
-	//	age := c.DefaultPostForm("age", "1")
-	//	c.JSON(200, gin.H{
-	//		"name":name,
-	//		"age":age,
-	//	})
-	//})`
-	//r.Run(":8080")
-
-	flag := make(chan bool)
-	c := cron.New();
-	c.AddFunc("*/1 * * * * *", func() {
-		fmt.Println("每秒")
+	r := gin.Default()
+	r.POST("/", func(c *gin.Context) {
+		name := c.DefaultPostForm("name", "ryan")
+		age := c.DefaultPostForm("age", "1")
+		c.JSON(200, gin.H{
+			"name":name,
+			"age":age,
+		})
 	})
-	c.Start()
-	<-flag
+	r.Run(":8080")
+
+	//flag := make(chan bool)
+	//c := cron.New();
+	//c.AddFunc("*/1 * * * * *", func() {
+	//	fmt.Println("每秒")
+	//})
+	//c.Start()
+	//<-flag
 }
